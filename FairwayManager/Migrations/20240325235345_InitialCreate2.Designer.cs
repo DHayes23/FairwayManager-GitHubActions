9263e0a795bbf38e219e58a3d94c284fc1c21c5f
DHayes23 <d.hayesdeveloper@gmail.com>
2024-03-25 23:55:01 +0000

    Re-initialise db

--- FairwayManager/Migrations/20240325235345_InitialCreate2.Designer.cs
+++ FairwayManager/Migrations/20240325235345_InitialCreate2.Designer.cs
@@ -2,6 +2,7 @@
 using FairwayManager.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FairwayManager.Migrations
 {
     [DbContext(typeof(FairwayManagerDBContext))]
-    partial class FairwayManagerDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240325235345_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.17");
