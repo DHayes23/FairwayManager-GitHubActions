a0ad795d8218b0ea0ca8bcdb569a2873deeb6bde
DHayes23 <d.hayesdeveloper@gmail.com>
2024-04-02 01:01:18 +0100

    Add sorting to menu screen

--- FairwayManager/Pages/Members/Index.cshtml.cs
+++ FairwayManager/Pages/Members/Index.cshtml.cs
@@ -18,14 +18,60 @@ namespace FairwayManager.Pages.Members
             _context = context;
         }
 
-        public IList<Member> Member { get;set; } = default!;
+        public IList<Member> Member { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public string MembershipIDSortParm { get; set; }
+        public string NameSortParm { get; set; }
+        public string SexSortParm { get; set; }
+        public string EmailSortParm { get; set; }
+        public string HandicapSortParm { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Members != null)
+            MembershipIDSortParm = String.IsNullOrEmpty(sortOrder) ? "membership_id_desc" : "";
+            NameSortParm = sortOrder == "Name" ? "name_desc" : "Name";
+            SexSortParm = sortOrder == "Sex" ? "sex_desc" : "Sex";
+            EmailSortParm = sortOrder == "Email" ? "email_desc" : "Email";
+            HandicapSortParm = sortOrder == "Handicap" ? "handicap_desc" : "Handicap";
+
+            IQueryable<Member> memberIQ = from s in _context.Members
+                                          select s;
+
+            switch (sortOrder)
             {
-                Member = await _context.Members.ToListAsync();
+                case "name_desc":
+                    memberIQ = memberIQ.OrderByDescending(s => s.Name);
+                    break;
+                case "Name":
+                    memberIQ = memberIQ.OrderBy(s => s.Name);
+                    break;
+                case "sex_desc":
+                    memberIQ = memberIQ.OrderByDescending(s => s.Sex);
+                    break;
+                case "Sex":
+                    memberIQ = memberIQ.OrderBy(s => s.Sex);
+                    break;
+                case "email_desc":
+                    memberIQ = memberIQ.OrderByDescending(s => s.Email);
+                    break;
+                case "Email":
+                    memberIQ = memberIQ.OrderBy(s => s.Email);
+                    break;
+                case "handicap_desc":
+                    memberIQ = memberIQ.OrderByDescending(s => s.Handicap);
+                    break;
+                case "Handicap":
+                    memberIQ = memberIQ.OrderBy(s => s.Handicap);
+                    break;
+                case "membership_id_desc":
+                    memberIQ = memberIQ.OrderByDescending(s => s.MembershipID);
+                    break;
+                default:
+                    memberIQ = memberIQ.OrderBy(s => s.MembershipID);
+                    break;
             }
+
+            Member = await memberIQ.AsNoTracking().ToListAsync();
         }
     }
 }
